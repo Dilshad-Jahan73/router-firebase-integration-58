54ba616094f923e9add08c33cf5b9c7f2e41d527
Dilshad-Jahan73 <dilshadjahan73@gmail.com>
2022-04-17 10:24:32 +0600

    added onauth state change to persist the auth

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiS8dYXHfflkpfyP9DrlHjfoYDhu7zDpM",
+  authDomain: "router-firebase-integrat-77259.firebaseapp.com",
+  projectId: "router-firebase-integrat-77259",
+  storageBucket: "router-firebase-integrat-77259.appspot.com",
+  messagingSenderId: "469092984115",
+  appId: "1:469092984115:web:05b4014603fad1a7bdca46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
